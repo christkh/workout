d67025428b73d896b03c19bb125df5209d2743ec
Chris Huang <chriskhuang@gmail.com>
2022-02-03 16:22:24 -0800

    c

--- workout.py
+++ workout.py
@@ -1,21 +1,32 @@
+#TODO: Add a column for week, append out to either csv or googlesheet
+
 import json
 import csv
 import datetime
 import pandas as pd
-import df2gspread as d2g
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 with open('message_1.json') as json_file:
     data = json.load(json_file)
 
-workout_data = data['messages']
+logdata = data["messages"]
 
+#TODO: use list comprehension
+pics = []
+for workout in logdata:
+    if 'photos' in workout.keys():
+        pics.append(workout)
+
+#writes to csv file
 data_file = open('workout_data.csv', 'w') 
 
 csv_writer = csv.writer(data_file) 
 
 count = 0
 
-for workout in workout_data:
+for workout in pics:
     if count == 0:
 
             header = workout.keys()
@@ -25,25 +36,23 @@ for workout in workout_data:
     csv_writer.writerow(workout.values())
 data_file.close()
 
-#converting csv file to panda dataframe
+# converting csv file to panda dataframe
 df = pd.read_csv('workout_data.csv')
 
 # converts the timestamp to datetime format
 df['timestamp_ms'] = pd.to_datetime(df.timestamp_ms, unit='ms')
+df['timestamp_ms'] = pd.to_datetime(df.timestamp_ms, format='%Y%m%d')
 
-# substring operation to pull in the first 8 characters
-df['photos'] = df['photos'].str.slice(0,8)
-
-# variable to stored filtered rows with matching substring
-pics = df[df['photos'].str.contains("'uri':")]
-
-print(pics)
-
-#print(df[['sender_name','timestamp_ms', 'pics']])
+# delete unnecessary columns
+df = df.drop(['photos', 'reactions', 'type', 'is_unsent'], axis=1)
+print(df)
 
 
-"""
-todo
-1)  load df into csv or googlesheet
+#load data to googlesheet
+#gc = gspread.service_account(filename=r'C:\Users\chris\workout\jsonFileFromGoogle.json')
 
-"""
+#sh = gc.open_by_key('1Utqjn3OIy-5UB3cZtK3DAyqVYkCFQ17VqUwrte1aqm0')
+
+#worksheet = sh.worksheet("Log2")
+
+#set_with_dataframe(worksheet, df)
